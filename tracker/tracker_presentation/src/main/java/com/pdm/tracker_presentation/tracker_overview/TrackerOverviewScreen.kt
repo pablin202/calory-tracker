eaf0d5b301493c0064aba4b7911963eee4bc7043
Pablo Molina <pablomolina@PabloMolinaMBP.gateway>
2023-03-02 10:37:43 +1100

    expandable meal UI

--- tracker/tracker_presentation/src/main/java/com/pdm/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/pdm/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -2,6 +2,7 @@ package com.pdm.tracker_presentation.tracker_overview
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -9,6 +10,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.pdm.core.util.UiEvent
 import com.pdm.core_ui.LocalSpacing
 import com.pdm.tracker_presentation.tracker_overview.components.DaySelector
+import com.pdm.tracker_presentation.tracker_overview.components.ExpandableMeal
 import com.pdm.tracker_presentation.tracker_overview.components.NutrientsHeader
 
 @Composable
@@ -40,6 +42,15 @@ fun TrackerOverviewScreen(
                     .padding(horizontal = spacing.spaceMedium)
             )
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+
+        items(state.meals) { item: Meal ->
+            ExpandableMeal(
+                meal = item,
+                onToggleClick = { viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(item)) },
+                content = { /*TODO*/ },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }

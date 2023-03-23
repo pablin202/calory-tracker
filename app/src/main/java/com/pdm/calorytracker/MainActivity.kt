399c2b1c3e551d53f840fae609a132c6d1cf79a4
Pablo Molina <pablomolina@PabloMolinaMBP.gateway>
2023-03-23 11:47:27 +1100

    search text ui

--- app/src/main/java/com/pdm/calorytracker/MainActivity.kt
+++ app/src/main/java/com/pdm/calorytracker/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.pdm.calorytracker.navigation.navigate
 import com.pdm.calorytracker.ui.theme.CaloryTrackerTheme
 import com.pdm.core.navigation.Route
@@ -23,11 +26,13 @@ import com.pdm.onboarding_presentation.height.HeightScreen
 import com.pdm.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.pdm.onboarding_presentation.weight.WeightScreen
 import com.pdm.onboarding_presentation.welcome.WelcomeScreen
+import com.pdm.tracker_presentation.search.SearchScreen
 import com.pdm.tracker_presentation.tracker_overview.TrackerOverviewScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalComposeUiApi::class)
     @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -79,6 +84,38 @@ class MainActivity : ComponentActivity() {
                         }
                         composable(Route.TRACKER_OVERVIEW) {
                             TrackerOverviewScreen(onNavigate = navController::navigate)
+                        }
+                        composable(
+                            route = Route.SEARCH + "/{mealName}/{dayOfMonth}/{month}/{year}",
+                            arguments = listOf(
+                                navArgument("mealName") {
+                                    type = NavType.StringType
+                                },
+                                navArgument("dayOfMonth") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("month") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("year") {
+                                    type = NavType.IntType
+                                },
+                            )
+                        ) {
+                            val mealName = it.arguments?.getString("mealName")!!
+                            val dayOfMonth = it.arguments?.getInt("dayOfMonth")!!
+                            val month = it.arguments?.getInt("month")!!
+                            val year = it.arguments?.getInt("year")!!
+                            SearchScreen(
+                                scaffoldState = scaffoldState,
+                                mealName = mealName,
+                                dayOfMonth = dayOfMonth,
+                                month = month,
+                                year = year,
+                                onNavigateUp = {
+                                    navController.navigateUp()
+                                }
+                            )
                         }
                     }
                 }
